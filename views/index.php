5f99a29bcc78c242cd7698abe84eccf32a6df59c
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2024-10-21 02:40:09 +0300

    styles: minified styles

--- views/index.php
+++ views/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Event Management System</title>
-    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="/styles/style.min.css">
 </head>
 <body class="bg-dark-navy text-white">
     <main class="min-h-full-view my-10">
